472148bdc83fe9f4cb4cb81ebf54b0e74c6b0eba
shtrihh88 <shtrihh88@mail.ru>
2021-09-08 00:33:33 +0300

    models was added

--- api_yamdb/reviews/models.py
+++ api_yamdb/reviews/models.py
@@ -2,8 +2,12 @@ from django.db import models
 
 
 class Category(models.Model):
-    name = models.CharField(verbose_name='category', max_length=50)
-    slug = models.SlugField(unique=True, max_length=50)
+    name = models.CharField(verbose_name='category', max_length=256)
+    slug = models.SlugField(
+        verbose_name='category slug',
+        unique=True,
+        max_length=50
+    )
 
     class Meta:
         ordering = ('slug',)
@@ -14,7 +18,11 @@ class Category(models.Model):
 
 class Genre(models.Model):
     name = models.CharField(verbose_name='genre', max_length=50)
-    slug = models.SlugField(unique=True, max_length=50)
+    slug = models.SlugField(
+        verbose_name='genre slug',
+        unique=True,
+        max_length=50
+    )
 
     class Meta:
         ordering = ('slug',)
@@ -24,15 +32,24 @@ class Genre(models.Model):
 
 
 class Title(models.Model):
-    name = models.CharField(verbose_name='title', max_length=200)
-    year = models.IntegerField()
-    genre = models.ManyToManyField(Genre, related_name='titles')
+    name = models.CharField(verbose_name='title', max_length=256)
+    year = models.IntegerField(verbose_name='year of issue')
+    description = models.TextField(
+        verbose_name='description',
+        blank=True,
+        null=True,
+    )
+    genre = models.ManyToManyField(
+        Genre,
+        related_name='titles',
+        verbose_name='genre title'
+    )
     category = models.ForeignKey(
         Category,
         on_delete=models.SET_NULL,
         null=True,
-        blank=True,
-        related_name='titles'
+        related_name='titles',
+        verbose_name='category title'
     )
 
     class Meta:
