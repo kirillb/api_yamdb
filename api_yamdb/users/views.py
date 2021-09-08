862fef72195bebc163ea14a1b9196fa37e39424c
Kirill Barannikov <kirillb@gmail.com>
2021-09-08 07:07:23 +0000

    Remove old code for signup and token generation

--- api_yamdb/users/views.py
+++ api_yamdb/users/views.py
@@ -3,7 +3,7 @@ from django.core.mail import send_mail
 from django.shortcuts import get_object_or_404
 
 from rest_framework import status, viewsets
-from rest_framework.decorators import api_view, action, permission_classes
+from rest_framework.decorators import action
 from rest_framework.permissions import AllowAny, IsAuthenticated
 from rest_framework.response import Response
 from rest_framework_simplejwt.tokens import AccessToken
@@ -47,71 +47,6 @@ class UserViewSet(viewsets.ModelViewSet):
         serializer.save(role=request.user.role)
 
         return Response(serializer.data)
-
-
-@api_view(['POST'])
-@permission_classes([AllowAny])
-def gen_confirmation_code(request):
-
-    serializer = SignupSerializer(data=request.data)
-    serializer.is_valid(raise_exception=True)
-
-    email = request.data.get('email')
-    username = request.data.get('username')
-
-    if username == 'me':
-        return Response(
-            {'username': ['username "me" is not allowed']},
-            status=status.HTTP_400_BAD_REQUEST
-        )
-
-    user, is_created = User.objects.get_or_create(
-        email=email,
-        username=username
-    )
-
-    if is_created:
-        user.is_active = False
-        user.save()
-
-    confirmation_code = code_generator.make_token(user)
-
-    send_mail(
-        'Confirmation code - API tamdb',
-        f'Confirmation code is {confirmation_code}',
-        None,
-        (email, )
-    )
-
-    return Response(
-        {'email': email, 'username': username},
-        status=status.HTTP_200_OK
-    )
-
-
-@api_view(['POST'])
-@permission_classes([AllowAny])
-def gen_access_token(request):
-
-    serializer = GenTokenSerializer(data=request.data)
-    serializer.is_valid(raise_exception=True)
-    confirmation_code = serializer.validated_data.get('confirmation_code')
-
-    user = get_object_or_404(
-        User,
-        username=serializer.validated_data.get('username')
-    )
-
-    if not code_generator.check_token(user, confirmation_code):
-        return Response(
-            {'confirmation_code': ['Code is not recognized']},
-            status=status.HTTP_400_BAD_REQUEST
-        )
-
-    user.is_active = True
-    user.save()
-
-    return Response({'token': str(AccessToken.for_user(user))})
 
 
 class SignupView(APIView):
