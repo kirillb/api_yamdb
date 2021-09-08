90eff5fca8c700737f396bf668856565c0aa5101
shtrihh88 <shtrihh88@mail.ru>
2021-09-08 18:24:44 +0300

    viewset was changed

--- api_yamdb/api/views.py
+++ api_yamdb/api/views.py
@@ -1,28 +1,39 @@
-from django_filters.rest_framework import DjangoFilterBackend
-from rest_framework import viewsets, filters
-from users.permissions import IsAdmin, IsAdminOrReadOnly
+from rest_framework import viewsets, filters, mixins, permissions
+from rest_framework.pagination import PageNumberPagination
+
+from users.permissions import IsAdminOrReadOnly
 
 from reviews.models import Category, Genre, Title
 from .serializers import CategorySerializer, GenreSerializer, TitleSerializers
 
 
-class CategoryViewSet(viewsets.ModelViewSet):
+class CreateListDestroy(mixins.CreateModelMixin,
+                        mixins.ListModelMixin,
+                        mixins.DestroyModelMixin,
+                        viewsets.GenericViewSet):
+    pass
+
+
+class CategoryViewSet(CreateListDestroy):
     queryset = Category.objects.all()
     serializer_class = CategorySerializer
     permission_classes = (IsAdminOrReadOnly,)
     filter_backends = (filters.SearchFilter,)
     search_fields = ('name',)
+    lookup_field = 'slug'
 
 
-class GenreViewSet(viewsets.ModelViewSet):
+class GenreViewSet(CreateListDestroy):
     queryset = Genre.objects.all()
     serializer_class = GenreSerializer
     permission_classes = (IsAdminOrReadOnly,)
     search_fields = ('name',)
     filter_backends = (filters.SearchFilter,)
+    lookup_field = 'slug'
 
 
 class TitleViewSet(viewsets.ModelViewSet):
     queryset = Title.objects.all()
     serializer_class = TitleSerializers
+    pagination_class = PageNumberPagination
     permission_classes = (IsAdminOrReadOnly,)
