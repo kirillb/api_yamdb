0d2d949ff8455e30512662fcf810a8aebd3b87e4
shtrihh88 <shtrihh88@mail.ru>
2021-09-11 14:14:33 +0300

    next try

--- api_yamdb/reviews/validators.py
+++ api_yamdb/reviews/validators.py
@@ -1,9 +1,9 @@
-import datetime
+from django.utils import timezone
 from rest_framework.exceptions import ValidationError
 
 
 def year_validator(value):
-    if value < 1900 or value > datetime.datetime.now().year:
+    if value < 1895 or value > timezone.now().year:
         raise ValidationError(
             f'{value} is not a correcrt year!',
             params={'value': value},
